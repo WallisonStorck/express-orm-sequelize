df66ba4bf1d9c848e0c699f61f1f81f1858ae411
Wallison Storck <storck779@gmail.com>
2022-09-09 20:08:37 -0400

    CRUD people finishe

--- controllers/PeopleController.js
+++ controllers/PeopleController.js
@@ -9,6 +9,67 @@ class PeopleController {
       return res.status(500).json(error.message);
     }
   }
+
+  static async findPerson(req, res) {
+    const { id } = req.params;
+    try {
+      const person = await database.People.findOne({
+        where: { id: Number(id) },
+      });
+      return res.status(200).json(person);
+    } catch (error) {
+      return res.status(500).json(error.message);
+    }
+  }
+
+  static async insertPerson(req, res) {
+    const newPerson = req.body;
+
+    try {
+      const newPersonEntered = await database.People.create(newPerson);
+      return res.status(200).json(newPersonEntered);
+    } catch (error) {
+      return res.status(500).json(error.message);
+    }
+  }
+
+  static async updatePerson(req, res) {
+    const { id } = req.params;
+    const newInfo = req.body;
+
+    try {
+      await database.People.update(newInfo, { where: { id: Number(id) } }); //Atualiza pessoa
+      const personUpdated = await database.People.findOne({
+        //Procura pessoa atualizada para mostrar no retorno do método
+        where: { id: Number(id) },
+      });
+      return res.status(200).json(personUpdated);
+    } catch (error) {
+      return res.status(500).json(error.message);
+    }
+  }
+
+  static async deletePerson(req, res) {
+    try {
+      const { id } = req.params;
+
+      const personExists = await database.People.findOne({
+        where: { id: Number(id) },
+      });
+
+      if (!personExists) throw new Error(`Pessoa ID:${id} não encontrada!`);
+
+      const rowDeleted = await database.People.destroy({
+        where: { id: Number(id) },
+      });
+
+      if (!rowDeleted) throw new Error("Erro inesperado!");
+
+      return res.status(200).json(`Pessoa ID:${id} excluída com sucesso!`);
+    } catch (error) {
+      return res.status(500).json(error.message);
+    }
+  }
 }
 
 module.exports = PeopleController;
--- routes/peopleRouter.js
+++ routes/peopleRouter.js
@@ -4,5 +4,9 @@ const PeopleController = require("../controllers/PeopleController");
 const router = Router();
 
 router.get("/people", PeopleController.getAllPeople);
+router.get("/people/:id", PeopleController.findPerson);
+router.post("/people", PeopleController.insertPerson);
+router.put("/people/:id", PeopleController.updatePerson);
+router.delete("/people/:id", PeopleController.deletePerson);
 
 module.exports = router;
